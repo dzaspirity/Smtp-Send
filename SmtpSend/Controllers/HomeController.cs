351ad6b83c5638aa72a30f3b931ffe7eb6126a57
Denis Zavershinskiy <zaverden@gmail.com>
2016-04-23 19:33:54 +0700

    send messages

--- SmtpSend/Controllers/HomeController.cs
+++ SmtpSend/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using SmtpSend.Models;
 
 namespace SmtpSend.Controllers
 {
@@ -11,6 +12,41 @@ namespace SmtpSend.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [ActionName(nameof(Index))]
+        [HttpPost]
+        public async Task<ActionResult> IndexPost(SendMessageModel model)
+        {
+            ViewBag.ShowSuccess = false;
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var smtpClient = new SmtpClient(model.SmtpHost, model.SmtpPort)
+            {
+                Credentials = new NetworkCredential(model.UserName, model.Password)
+            };
+
+            var mailMessage = new MailMessage(model.EmailFrom, model.EmailTo)
+            {
+                Body = model.Body,
+                Subject = model.Subject,
+                IsBodyHtml = false
+            };
+
+            try
+            {
+                await smtpClient.SendMailAsync(mailMessage);
+                ViewBag.ShowSuccess = true;
+            }
+            catch (Exception exc)
+            {
+                ModelState.AddModelError(nameof(model.SmtpHost), exc);
+            }
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ SmtpSend/Models/SendMessageModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmtpSend.Models
+{
+    public class SendMessageModel
+    {
+        [Required]
+        [Display(Name = "SMTP Host")]
+        public string SmtpHost { get; set; }
+
+        [Required]
+        [Display(Name = "SMTP Port")]
+        public int SmtpPort { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email From")]
+        public string EmailFrom { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email To")]
+        public string EmailTo { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
